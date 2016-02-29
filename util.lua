4785fb8b99d028f4faccff69ea203b02b7c9bc55
noogen <tom@noogen.net>
2016-02-29 14:53:21 -0600

    add slugemail

--- util.lua
+++ util.lua
@@ -13,4 +13,16 @@ function slugify(str)
 	return string.lower(string.gsub(string.gsub(str,"[^ A-Za-z]"," "),"[ ]+","-"))
 end
 
-return { slugify = slugify, trim = trim }
+function slugemail(str)
+	if (str == nil) then
+		return nil
+	end
+	str = trim(str)
+	return string.lower(string.gsub(str,"[^@0-9A-Za-z]","-"))
+end
+
+return { 
+	slugify = slugify, 
+	trim = trim,
+	slugemail = slugemail 
+}
