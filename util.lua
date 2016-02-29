3d335a16d64d4ae228be1ab95944a3b5d3a2481b
noogen <tom@noogen.net>
2016-02-29 12:06:31 -0600

    slugify

--- util.lua
+++ util.lua
@@ -2,7 +2,7 @@ function slugify(str)
 	if (str == nil) then
 		return nil
 	end
-	return string.lower(string.gsub(string.gsub(str,"[^ A-Za-z]",""),"[ ]+","-"))
+	return string.lower(string.gsub(string.gsub(str,"[^ A-Za-z]"," "),"[ ]+","-"))
 end
 
 return { slugify = slugify }
