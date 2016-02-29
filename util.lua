eda955ca8cee279f55fbe5dde493c53d8dbea86c
noogen <tom@noogen.net>
2016-02-29 13:37:15 -0600

    update

--- util.lua
+++ util.lua
@@ -1,8 +1,16 @@
+function trim(str)
+	if (str == nil) then
+		return nil
+	end
+	return str:match'^%s*(.*%S)' or ''
+end
+
 function slugify(str)
 	if (str == nil) then
 		return nil
 	end
+	str = trim(str)
 	return string.lower(string.gsub(string.gsub(str,"[^ A-Za-z]"," "),"[ ]+","-"))
 end
 
-return { slugify = slugify }
+return { slugify = slugify, trim = trim }
